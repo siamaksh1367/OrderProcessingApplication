bc1e9662488f0697b606c5473540bd916f97fa9d
edsish <sish@energidanmark.dk>
2021-07-10 17:32:37 +0200

    add new PaymentEventargs and OrderEventArgs and Refactor the code in seperate files

---
+++ OrderProcessor/PayementEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrderProcessor
+{
+    public class PayementEventArgs: EventArgs
+    {
+        public PayementEventArgs(Peyment payment)
+        {
+
+        }
+    }
+}
