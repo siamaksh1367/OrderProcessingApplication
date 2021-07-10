62bfc2b904f6d85ee694c1429975e756baa79881
edsish <sish@energidanmark.dk>
2021-07-10 18:37:33 +0200

    Add a new project to be able to execute the program

---
+++ OrderProcessRunner/Program.cs
@@ -0,0 +1,14 @@
+﻿using OrderProcessor;
+using System;
+
+namespace OrderProcessRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var orderProc = new OrderProcessorSimple(new PaymentProcessorPhysicalPruduct());
+            orderProc.ProcessOrder(new Order(){ OrderNumber=1});
+        }
+    }
+}
