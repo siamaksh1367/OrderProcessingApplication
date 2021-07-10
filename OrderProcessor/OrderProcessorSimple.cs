4e6239d2f4835fe2862460517e8ba354695c9b2a
edsish <sish@energidanmark.dk>
2021-07-10 18:36:57 +0200

    add  concrete class holding the ipmlementation for OrderProcessor

---
+++ OrderProcessor/OrderProcessorSimple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OrderProcessor
+{
+    public class OrderProcessorSimple : OrderProcessor
+    {
+        public OrderProcessorSimple(PaymentProcessor paymentProcessor):base(paymentProcessor)
+        {
+            
+        }
+        public override void ProcessOrder(Order order)
+        {
+            Console.WriteLine("Processing the order just started");
+            OnOrderProcessing(order);
+        }
+    }
+}
+
+
