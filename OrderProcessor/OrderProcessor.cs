865b83da3d8f7a2a40d37151a5fc530859b83c8d
edsish <sish@energidanmark.dk>
2021-07-10 18:33:16 +0200

    Move The logic for subscription into the parent calss to avoid duplicate code in every derived cless

--- OrderProcessor/OrderProcessor.cs
+++ OrderProcessor/OrderProcessor.cs
@@ -4,6 +4,11 @@ namespace OrderProcessor
 {
     public abstract class OrderProcessor 
     {
+
+        public OrderProcessor(PaymentProcessor paymentProcessor)
+        {
+            OrderProcessing += paymentProcessor.OnOrderProcessing;
+        }
         public event EventHandler<OrderEventArgs> OrderProcessing;
         public abstract void ProcessOrder(Order order);
         protected virtual void OnOrderProcessing(Order order) 
