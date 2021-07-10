e7e3cb2ccf7e404043091d321d33dc776dfaec48
edsish <sish@energidanmark.dk>
2021-07-10 18:38:02 +0200

    Creat a subscriber to the order processor class

---
+++ OrderProcessor/PaymentProcessorPhysicalPruduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OrderProcessor
+{
+    public class PaymentProcessorPhysicalPruduct : PaymentProcessor
+    {
+        public override void OnOrderProcessing(object sender, OrderEventArgs order)
+        {
+            Console.WriteLine("Payment process finished for a Physical Pruduct ");
+            var payment = new Payment();
+            OnPaymentProcessed(payment);
+        }
+    }
+}
