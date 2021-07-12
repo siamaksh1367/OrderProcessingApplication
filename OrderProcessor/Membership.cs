341ba10523cb3efdf83641637da77980506591fe
edsish <sish@energidanmark.dk>
2021-07-12 08:13:47 +0200

    define a simple concerete observer

---
+++ OrderProcessor/Membership.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessor
+{
+
+    public class Membership : IObserver<Payment>
+    {
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(Payment value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
