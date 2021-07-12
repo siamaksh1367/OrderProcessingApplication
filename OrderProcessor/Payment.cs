8402e7a701dba6ce27317dfe584061167086aab0
edsish <sish@energidanmark.dk>
2021-07-12 08:13:05 +0200

    define a payment calss as a subject to implement observer design pattern

---
+++ OrderProcessor/Payment.cs
@@ -0,0 +1,16 @@
+﻿namespace OrderProcessor
+{
+    public class Payment
+    {
+        private readonly PaymentType _paymentType;
+
+
+        public Payment(PaymentType paymentType)
+        {
+            this._paymentType = paymentType;
+        }
+
+        public PaymentType PaymentType{ get { return _paymentType} private set { } }
+
+    }
+}
